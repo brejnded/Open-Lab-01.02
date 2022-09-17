e6670b76d46c1dc68a4e314d79db4928d82e87c7
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-09-17 22:16:50 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,12 +1,20 @@
-﻿using System;
+sing System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Open_Lab_01._02
 {
     public class Exercise
     {
-        public int Sum(int a, int b)
+        public int Sum(int x, int y)
         {
+            return x + y;
             throw new NotImplementedException();
         }
     }
 }
+
+
+
+
+
+           
